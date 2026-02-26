f5d05907bc8b9b6f140738512eea1ac8ef086596
raha mohebbi <raha.mohebbi.03@gmail.com>
2026-02-26 21:16:07 +0330

    fix : positions of the icons

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -11,13 +11,13 @@ const SideBar = () => {
     <div className="sidebar flex flex-col gap-4 p-5 bg-gray-800 text-white w-[12.5rem]">
        
       <nav className="nav flex flex-col gap-3">
-        <a href="#" className="nav-item flex hover:bg-gray-700 p-2 rounded"><TiHomeOutline className="mr-2" />Dashboard</a>
-        <a href="#" className="nav-item flex hover:bg-gray-700 p-2 rounded"> <LuUsers className="mr-2" />Users</a>
-        <a href="#" className="nav-item flex hover:bg-gray-700 p-2 rounded"><FaRegHeart className="mr-2" />Campaigns</a>
-        <a href="#" className="nav-item flex hover:bg-gray-700 p-2 rounded"><HiOutlineDocumentDuplicate className="mr-2" />Documents</a>
-        <a href="#" className="nav-item flex hover:bg-gray-700 p-2 rounded"><RiSettings2Line className="mr-2" />Settings</a>
-                <a href="#" className="nav-item flex hover:bg-gray-700 p-2 rounded"><LuCircleHelp className="mr-2" />Help</a>
-                        <a href="#" className="nav-item flex hover:bg-gray-700 p-2 rounded"><IoLogOutOutline className="mr-2" />Logout</a>
+        <a href="#" className="nav-item flex hover:bg-gray-700 p-2 rounded"><TiHomeOutline className="mr-5 mt-1" />Dashboard</a>
+        <a href="#" className="nav-item flex hover:bg-gray-700 p-2 rounded"> <LuUsers className="mr-5 mt-1" />Users</a>
+        <a href="#" className="nav-item flex hover:bg-gray-700 p-2 rounded"><FaRegHeart className="mr-5 mt-1" />Campaigns</a>
+        <a href="#" className="nav-item flex hover:bg-gray-700 p-2 rounded"><HiOutlineDocumentDuplicate className="mr-5 mt-1" />Documents</a>
+        <a href="#" className="nav-item flex hover:bg-gray-700 p-2 rounded"><RiSettings2Line className="mr-5 mt-1" />Settings</a>
+                <a href="#" className="nav-item flex hover:bg-gray-700 p-2 rounded"><LuCircleHelp className="mr-5 mt-1" />Help</a>
+                        <a href="#" className="nav-item flex hover:bg-gray-700 p-2 rounded"><IoLogOutOutline className="mr-5 mt-1" />Logout</a>
       </nav>
     </div>
   )}
