d3895768cc41798940d4861ef972ad7e47823dc5
raha mohebbi <raha.mohebbi.03@gmail.com>
2026-02-26 22:43:03 +0330

    only spaces ! :)))

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,37 +2,42 @@ import { RiNotification2Line } from "react-icons/ri";
 import { TbSearch } from "react-icons/tb";
 
 const Navbar = () => {
-    return (
-        <div className="navbar bg-white flex items-center p-1 shadow-md">
-            <h1 className="text-xl font-bold text-[#60d680] mr-4 ml-8">Charity</h1>
-            <div className="search flex items-center gap-2 border-l pl-4 ml-20">
-                <div className="flex items-center gap-2 border rounded-md px-3 py-1">
-                    <TbSearch className="text-xl text-gray-500" />
-                    <input
-                        type="text"
-                        placeholder="Search anything..."
-                        className="border-none outline-none p-1 w-64"
-                    />
-                </div>
-            </div>
-            <div className="nav-links flex gap-6 items-center ml-auto">
-                <div className="bg-black rounded-md w-8 h-8 flex items-center justify-center text-white text-xs">
-                    <a href="#" className="text-white ">
-                        <RiNotification2Line className="w-5 h-5" />
-                    </a>
-                </div>
-                <a href="#" className="text-gray-500">User Panel</a>
-                {/* profile photo */}
-                <a href="#" className="w-10 h-10 rounded-full overflow-hidden border-2 border-gray-300">
-                    <img
-                        src="https://via.placeholder.com/150"
-                        alt="Profile"
-                        className="w-full h-full object-cover"
-                    />
-                </a>
-            </div>
+  return (
+    <div className="navbar bg-white flex items-center p-1 shadow-md">
+      <h1 className="text-xl font-bold text-[#60d680] mr-4 ml-8">Charity</h1>
+      <div className="search flex items-center gap-2 border-l pl-4 ml-20">
+        <div className="flex items-center gap-2 border rounded-md px-3 py-1">
+          <TbSearch className="text-xl text-gray-500" />
+          <input
+            type="text"
+            placeholder="Search anything..."
+            className="border-none outline-none p-1 w-64"
+          />
         </div>
-    );
+      </div>
+      <div className="nav-links flex gap-6 items-center ml-auto">
+        <div className="bg-black rounded-md w-8 h-8 flex items-center justify-center text-white text-xs">
+          <a href="#" className="text-white ">
+            <RiNotification2Line className="w-5 h-5" />
+          </a>
+        </div>
+        <a href="#" className="text-gray-500">
+          User Panel
+        </a>
+        {/* profile photo */}
+        <a
+          href="#"
+          className="w-10 h-10 rounded-full overflow-hidden border-2 border-gray-300"
+        >
+          <img
+            src="https://via.placeholder.com/150"
+            alt="Profile"
+            className="w-full h-full object-cover"
+          />
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -8,17 +8,60 @@ import { IoLogOutOutline } from "react-icons/io5";
 
 const SideBar = () => {
   return (
-    <div className="sidebar flex flex-col gap-4 p-5 bg-gray-800 text-white w-[12.5rem]">
-       
+    <div className="sidebar flex flex-col gap-4 p-5 bg-white text-gray-500  w-[12.5rem]">
       <nav className="nav flex flex-col gap-3">
-        <a href="#" className="nav-item flex hover:bg-gray-700 p-2 rounded"><TiHomeOutline className="mr-5 mt-1" />Dashboard</a>
-        <a href="#" className="nav-item flex hover:bg-gray-700 p-2 rounded"> <LuUsers className="mr-5 mt-1" />Users</a>
-        <a href="#" className="nav-item flex hover:bg-gray-700 p-2 rounded"><FaRegHeart className="mr-5 mt-1" />Campaigns</a>
-        <a href="#" className="nav-item flex hover:bg-gray-700 p-2 rounded"><HiOutlineDocumentDuplicate className="mr-5 mt-1" />Documents</a>
-        <a href="#" className="nav-item flex hover:bg-gray-700 p-2 rounded"><RiSettings2Line className="mr-5 mt-1" />Settings</a>
-                <a href="#" className="nav-item flex hover:bg-gray-700 p-2 rounded"><LuCircleHelp className="mr-5 mt-1" />Help</a>
-                        <a href="#" className="nav-item flex hover:bg-gray-700 p-2 rounded"><IoLogOutOutline className="mr-5 mt-1" />Logout</a>
+        <a
+          href="./Dashboard.jsx"
+          className="nav-item flex hover:bg-black hover:text-white p-2 rounded"
+        >
+          <TiHomeOutline className="mr-5 mt-1" />
+          Dashboard
+        </a>
+        <a
+          href="#"
+          className="nav-item flex hover:bg-black hover:text-white p-2 rounded"
+        >
+          {" "}
+          <LuUsers className="mr-5 mt-1" />
+          Users
+        </a>
+        <a
+          href="#"
+          className="nav-item flex hover:bg-black hover:text-white p-2 rounded"
+        >
+          <FaRegHeart className="mr-5 mt-1" />
+          Campaigns
+        </a>
+        <a
+          href="#"
+          className="nav-item flex hover:bg-black hover:text-white p-2 rounded"
+        >
+          <HiOutlineDocumentDuplicate className="mr-5 mt-1" />
+          Documents
+        </a>
+        <a
+          href="#"
+          className="nav-item flex hover:bg-black hover:text-white p-2 rounded"
+        >
+          <RiSettings2Line className="mr-5 mt-1" />
+          Settings
+        </a>
+        <a
+          href="#"
+          className="nav-item flex hover:bg-black hover:text-white p-2 rounded"
+        >
+          <LuCircleHelp className="mr-5 mt-1" />
+          Help
+        </a>
+        <a
+          href="#"
+          className="nav-item flex hover:bg-black hover:text-white p-2 rounded"
+        >
+          <IoLogOutOutline className="mr-5 mt-1" />
+          Logout
+        </a>
       </nav>
     </div>
-  )}
-  export default SideBar
+  );
+};
+export default SideBar;
