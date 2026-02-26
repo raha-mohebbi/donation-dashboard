4c4c9950b262a746363f48e677f04f3825e4a508
raha mohebbi <raha.mohebbi.03@gmail.com>
2026-02-26 17:16:46 +0330

    feat: adding Navbar.js & SideBar.js to it

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
-
+import SideBar from './components/SideBar'
+import Navbar from './components/navbar'
 function App() {
 
 
   return (
     <>
       <div>
-   heloo
+        <Navbar />
       </div>
-    
+    <SideBar />
     </>
   )
 }
